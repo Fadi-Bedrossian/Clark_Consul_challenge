7b8997e3aef927244f82a10b69decbbc1f3fbc90
fadi-bedrossian <fadybedrossian@gmail.com>
2022-02-18 03:00:36 +0200

    modify in script name and path to install consul server

--- Consul_cluster/my_project/myserver/main.tf
+++ Consul_cluster/my_project/myserver/main.tf
@@ -3,5 +3,5 @@ module "ec2" {
   ec2_name  = var.ec2_name
   subnet_id = data.terraform_remote_state.vpc.outputs.private_subnets
   vpc_id    = data.terraform_remote_state.vpc.outputs.vpc_id
-  user_data = file("${path.module}/template/install_server_pkgs.tpl")
+  user_data = file("${path.module}/template/install_consul_server.tpl")
 }
